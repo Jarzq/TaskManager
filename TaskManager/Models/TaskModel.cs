c493067b00830e33ef626d7dbf75a9bf2d8fdbd0
Mateusz <matijarzebski@wp.pl>
2022-03-13 18:45:09 +0100

    Basic functionalities added

---
+++ TaskManager/Models/TaskModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Models
+{
+    [Table("Tasks")]
+    public class TaskModel
+    {
+        [Key]
+        public int TaskId { get; set; }
+        
+        [Required(ErrorMessage = "Required Name")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(2000)]
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
---
+++ TaskManager/Repositories/ITaskRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using TaskManager.Models;
+
+namespace TaskManager.Repositories
+{
+    public interface ITaskRepository
+    {
+        TaskModel Get(int taskId);
+        IQueryable<TaskModel> GetAllActive();
+
+        void Add(TaskModel task);
+
+        void Update(int taskId, TaskModel task);
+        void Delete(int taskId);
+    }
+}
